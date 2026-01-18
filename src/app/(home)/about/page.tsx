9db80e570b4b63a973a4d0a6a0cac4744a0d2ec2
PIEROLS15 <pierodanielllanossanchez@gmail.com>
2026-01-18 10:56:13 -0500

    fix: correction layout and styles

--- src/app/(home)/about/page.tsx
+++ src/app/(home)/about/page.tsx
@@ -1,25 +1,20 @@
 "use client";
 
-import { useState } from "react";
-
 import Header from "@/components/home/layout/header";
-import Footer from "@/components/home/layout/footer";
-
-import HeroSection from "@/components/home/about/HeroSection";
-import StatsGrid from "@/components/home/about/StatsGrid";
-import MissionVision from "@/components/home/about/MissionVision";
-import ValuesGrid from "@/components/home/about/ValuesGrid";
-import TeamGrid from "@/components/home/about/TeamGrid";
-import WhyChooseUs from "@/components/home/about/WhyChooseUs";
+import HeroSection from "@/components/home/about2/heroSection";
+import StatsGrid from "@/components/home/about2/statsGrid";
+import MissionVision from "@/components/home/about2/missionVision";
+import ValuesGrid from "@/components/home/about2/valuesGrid";
+import TeamGrid from "@/components/home/about2/teamGrid";
+import WhyChooseUs from "@/components/home/about2/whyChooseUs";
 
 const About = () => {
-  const [isChatOpen, setIsChatOpen] = useState(false);
 
   return (
     <div className="min-h-screen bg-background">
       <Header />
 
-      <main className="pt-20">
+      <main>
         <HeroSection />
         <StatsGrid />
         <MissionVision />
