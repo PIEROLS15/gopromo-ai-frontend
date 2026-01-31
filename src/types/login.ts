eaae33fccffce60d7213f9acbbb093d6c62eb27a
PIEROLS15 <pierodanielllanossanchez@gmail.com>
2026-01-31 08:47:01 -0500

    fix: types in loginresponse

--- src/types/login.ts
+++ src/types/login.ts
@@ -3,10 +3,34 @@ export interface LoginPayload {
   password: string;
 };
 
+export type UserRole = {
+  id: number;
+  name: string;
+};
+
+export type User = {
+  id: number;
+  email: string;
+  fullName: string;
+  educationalInstitution: string;
+  phone: string;
+  avatar: string | null;
+  role: UserRole;
+};
+
+export type Supplier = {
+  id: number;
+  email: string;
+  ruc: string;
+  representativeName: string;
+  companyName: string;
+  phone: string;
+  avatar: string | null;
+  active: boolean;
+  verified: boolean;
+  role: UserRole;
+};
+
 export type LoginResponse = {
-  user: {
-    id: number;
-    email: string;
-    name?: string;
-  };
+  user: User | Supplier;
 };
